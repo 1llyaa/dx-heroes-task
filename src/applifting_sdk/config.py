3c4104e26108b5faaf3cab8364a686ee50c245bc
1llyaa <illyamiloserdov@skaut.cz>
2025-08-10 17:12:44 +0200

    chore: Change name of env variable

--- src/applifting_sdk/config.py
+++ src/applifting_sdk/config.py
@@ -1,6 +1,6 @@
 import os
 
-BASE_URL = os.getenv("OFFERS_API_BASE_URL", "https://python.exercise.applifting.cz/")
+BASE_URL = os.environ.get("APPLIFT_SDK_API_BASE_URL", "https://python.exercise.applifting.cz/")
 
-TOKEN_EXPIRATION_SECONDS = int(os.getenv("TOKEN_EXPIRATION_SECONDS", 300))
-TOKEN_EXPIRATION_BUFFER_SECONDS = int(os.getenv("TOKEN_EXPIRATION_BUFFER_SECONDS", 5))
+TOKEN_EXPIRATION_SECONDS = int(os.environ.get("TOKEN_EXPIRATION_SECONDS", 300))
+TOKEN_EXPIRATION_BUFFER_SECONDS = int(os.environ.get("TOKEN_EXPIRATION_BUFFER_SECONDS", 5))
