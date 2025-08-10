4510ed1f83b8c28b7a4d015cb2371304b6c7e9aa
1llyaa <illyamiloserdov@skaut.cz>
2025-08-10 18:35:55 +0200

    feat: Improve config.py

--- src/applifting_sdk/config.py
+++ src/applifting_sdk/config.py
@@ -1,8 +1,9 @@
+from typing import Optional
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    refresh_token: str
+    refresh_token: Optional[str] = None
     base_url: str = "https://python.exercise.applifting.cz/"
     token_expiration_seconds: int = 300
     token_expiration_buffer_seconds: int = 5
