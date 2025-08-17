36c74bc398535721746ef809a34003af41045643
1llyaa <illyamiloserdov@skaut.cz>
2025-08-17 17:52:35 +0200

    feat: Add sync client

--- src/applifting_sdk/http/__init__.py
+++ src/applifting_sdk/http/__init__.py
@@ -1,3 +1,4 @@
 from applifting_sdk.http.async_client import AsyncBaseClient
+from applifting_sdk.http.sync_client import SyncBaseClient
 
-__all__ = ["AsyncBaseClient"]
+__all__ = ["AsyncBaseClient", "SyncBaseClient"]
